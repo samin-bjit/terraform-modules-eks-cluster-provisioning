c8367731d8c4185944762de04f8b618d8d922cb5
samin irtiza <samin.irtiza@bjitgroup.com>
2023-09-12 16:50:13 +0600

    Add RDS provisioning

--- main.tf
+++ main.tf
@@ -33,12 +33,14 @@ module "vpc" {
   cidr = "10.0.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 2)
 
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+  private_subnets              = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets               = ["10.0.3.0/24", "10.0.4.0/24"]
+  database_subnets             = ["10.0.5.0/24", "10.0.6.0/24"]
+  create_database_subnet_group = true
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
+  enable_nat_gateway      = true
+  single_nat_gateway      = true
+  enable_dns_hostnames    = true
   map_public_ip_on_launch = true # disable this in production for better security
 
   public_subnet_tags = {
@@ -112,10 +114,60 @@ resource "aws_eks_addon" "ebs-csi" {
 
 resource "aws_iam_policy" "eks-worker-nodegroup-policy" {
   name   = "${local.cluster_name}-worker"
-  policy = file("policies/eks-workers.json") # replace with the path to your IAM policy JSON file
+  policy = file("policies/eks-workers.json")
 }
 
 resource "aws_iam_role_policy_attachment" "nodegroup-attached-policy" {
   role       = module.eks.eks_managed_node_groups["one"].iam_role_name # replace with the name of your node group IAM instance profile
   policy_arn = aws_iam_policy.eks-worker-nodegroup-policy.arn
+}
+
+resource "aws_iam_policy" "eks-alb-policy" {
+  name   = "${local.cluster_name}-alb-policy"
+  policy = file("policies/eks-alb-policy.json")
+}
+# ----------------- RDS Provisioning --------------------
+
+module "db" {
+  source = "terraform-aws-modules/rds/aws"
+
+  identifier                = "vaccination-rds"
+  create_db_option_group    = false
+  create_db_parameter_group = false
+
+  # All available versions: http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_MySQL.html#MySQL.Concepts.VersionMgmt
+  engine               = "mysql"
+  engine_version       = "8.0"
+  family               = "mysql8.0" # DB parameter group
+  major_engine_version = "8.0"      # DB option group
+  instance_class       = "db.t3.micro"
+
+  allocated_storage = 20
+
+  db_name  = "vms"
+  username = "vms"
+  port     = 3306
+
+  db_subnet_group_name   = module.vpc.database_subnet_group
+  vpc_security_group_ids = [module.security_group.security_group_id]
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = "vaccination-rds-sg"
+  description = "Complete MySQL example security group"
+  vpc_id      = module.vpc.vpc_id
+
+  # ingress
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 3306
+      to_port     = 3306
+      protocol    = "tcp"
+      description = "MySQL access from within VPC"
+      cidr_blocks = module.vpc.vpc_cidr_block
+    },
+  ]
 }
